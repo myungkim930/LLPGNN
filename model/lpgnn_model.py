e1c8b529dfa399aade96bddc8780dabf82ad8c3d
myungkim930 <myung.kim@inria.fr>
2023-02-17 06:07:35 +0900

    initial commit

---
+++ model/lpgnn_model.py
@@ -0,0 +1,19 @@
+##### LPGNN model #####
+
+import torch
+import torch.nn as nn
+
+## LPGNN model definition
+
+class LPGNN_Layer(nn.Module):
+    def __init__(self):
+
+        super(LPGNN_Layer, self).__init__()
+
+
+
+
+    def forward():
+
+
+        
